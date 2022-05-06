4ce64ad9ef3d93ca6bf53402a300f5876a3c70b6
LeeadJ <leead123@gmail.com>
2022-05-06 15:41:55 +0300

    updated coup()

--- sources/Assassin.hpp
+++ sources/Assassin.hpp
@@ -5,11 +5,16 @@
 namespace coup{
     class Assassin : public Player{
 
+        std::stack<Player> _victimStack;
+
         public:
+        //Getters:
+        std::stack<Player> getVictimStack() const {return this->_victimStack;}
+
         //Constructor
         Assassin(const Game& game, const std::string& name);
 
         //Functions
-        // void coup(Player& p);
+        void coup(Player& p);
     };
 }
